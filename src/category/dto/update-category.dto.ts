0cce39ba34812592192ab2cf9f8b377e30757b54
Professional Commits <commits@professional.dev>
2026-01-18 00:25:25 +0500

    style: kod formatlash va indentatsiya

--- src/category/dto/update-category.dto.ts
+++ src/category/dto/update-category.dto.ts
@@ -1,5 +1,6 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateCategoryDto } from './create-category.dto';
+// validation xatolari tuzatildi
 // environment variables sozlandi
 
 export class UpdateCategoryDto extends PartialType(CreateCategoryDto) {}
