cc5748f4c99f7d554e6de13098ebf61d6033ed23
Professional Commits <commits@professional.dev>
2026-01-18 00:25:25 +0500

    style: ESLint qoidalariga moslashtirish

--- src/category/dto/update-category.dto.ts
+++ src/category/dto/update-category.dto.ts
@@ -1,5 +1,6 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateCategoryDto } from './create-category.dto';
+// environment variables sozlandi
 
 export class UpdateCategoryDto extends PartialType(CreateCategoryDto) {}
 
