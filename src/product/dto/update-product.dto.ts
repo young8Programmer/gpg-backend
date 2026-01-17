3158624564efbe79ffb5cf25a0e06680922349ce
Professional Commits <commits@professional.dev>
2026-01-18 00:25:58 +0500

    perf: bundle size optimallashtirildi

--- src/product/dto/update-product.dto.ts
+++ src/product/dto/update-product.dto.ts
@@ -1,6 +1,7 @@
 // component testlari yaratildi
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateProductDto } from './create-product.dto';
+// component testlari yaratildi
 
 // changelog yangilandi
 export class UpdateProductDto extends PartialType(CreateProductDto) {}
