de6c11c7d0977199a53a64486677da6f7d77b42c
Professional Commits <commits@professional.dev>
2026-01-18 00:26:17 +0500

    test: integration testlar yaratildi

--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -4,6 +4,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Product } from './entities/product.entity';
 import { CreateProductDto } from './dto/create-product.dto';
+// database querylarni optimallashtirish
 import { UpdateProductDto } from './dto/update-product.dto';
 import { FileUploadService } from '../common/services/file-upload.service';
 import { CategoryService } from '../category/category.service';
