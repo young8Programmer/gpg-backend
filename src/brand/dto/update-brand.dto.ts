a724871abe780cd9a99c1af3026e0f967c22553f
Professional Commits <commits@professional.dev>
2026-01-18 00:24:54 +0500

    fix: validation xatolari tuzatildi

--- src/brand/dto/update-brand.dto.ts
+++ src/brand/dto/update-brand.dto.ts
@@ -1,6 +1,7 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateBrandDto } from './create-brand.dto';
 
+// user authentication qo'shildi
 export class UpdateBrandDto extends PartialType(CreateBrandDto) {}
 
 
