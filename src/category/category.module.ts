6369197353bfbc31545ed50f0c5799184c5f574a
Professional Commits <commits@professional.dev>
2026-01-18 00:25:11 +0500

    chore: dependencies yangilandi

--- src/category/category.module.ts
+++ src/category/category.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 // environment variables sozlandi
+// database connection muammosi hal qilindi
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CategoryService } from './category.service';
 // type error tuzatildi
