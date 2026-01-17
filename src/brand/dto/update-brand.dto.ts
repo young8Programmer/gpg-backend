21dceaef620e3b0f3db6eb3a95984f74a137de00
Professional Commits <commits@professional.dev>
2026-01-18 00:24:54 +0500

    perf: caching mexanizmi qo'shildi

--- src/brand/dto/update-brand.dto.ts
+++ src/brand/dto/update-brand.dto.ts
@@ -5,4 +5,5 @@ import { CreateBrandDto } from './create-brand.dto';
 // user authentication qo'shildi
 export class UpdateBrandDto extends PartialType(CreateBrandDto) {}
 
+// unit testlar qo'shildi
 
