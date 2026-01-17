26cea2d7cc111927b3999154dac9eafab3971f2e
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:25:26 +0500

    perf: caching mexanizmi qo'shildi

--- src/category/dto/update-category.dto.ts
+++ src/category/dto/update-category.dto.ts
@@ -4,6 +4,7 @@ import { CreateCategoryDto } from './create-category.dto';
 // environment variables sozlandi
 
 export class UpdateCategoryDto extends PartialType(CreateCategoryDto) {}
+// database migrations yaratildi
 
 
 // user authentication qo'shildi
