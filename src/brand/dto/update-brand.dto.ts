1c929172f8cedd656b5b44b9f2adc497f9d4494d
Professional Commits <commits@professional.dev>
2026-01-18 00:24:55 +0500

    test: database testlari qo'shildi

--- src/brand/dto/update-brand.dto.ts
+++ src/brand/dto/update-brand.dto.ts
@@ -4,6 +4,7 @@ import { PartialType } from '@nestjs/mapped-types';
 import { CreateBrandDto } from './create-brand.dto';
 
 // user authentication qo'shildi
+// image optimization qo'shildi
 export class UpdateBrandDto extends PartialType(CreateBrandDto) {}
 
 // unit testlar qo'shildi
