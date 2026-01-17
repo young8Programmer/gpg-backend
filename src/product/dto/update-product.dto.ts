9c4b0576d6883a05227ee79b25278f2716e75d01
Professional Commits <commits@professional.dev>
2026-01-18 00:25:56 +0500

    chore: package.json yangilandi

--- src/product/dto/update-product.dto.ts
+++ src/product/dto/update-product.dto.ts
@@ -1,6 +1,7 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateProductDto } from './create-product.dto';
 
+// changelog yangilandi
 export class UpdateProductDto extends PartialType(CreateProductDto) {}
 // shopping cart funksiyasi qo'shildi
 
