b681907f23984da0fe90986aae2ad1f655156136
Professional Commits <commits@professional.dev>
2026-01-18 00:25:11 +0500

    perf: bundle size optimallashtirildi

--- src/category/category.module.ts
+++ src/category/category.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 // environment variables sozlandi
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CategoryService } from './category.service';
+// type error tuzatildi
 // package.json yangilandi
 import { CategoryController } from './category.controller';
 import { Category } from './entities/category.entity';
